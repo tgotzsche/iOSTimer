8361d9b7cfe1e645cf52a83e976a68f5fd9d6fe0
Thomas Gotzsche <tgotzsche@gmail.com>
2017-11-05 18:03:06 +0100

    Initial Commit

---
+++ iOSTimer/iOSTimer.h
@@ -0,0 +1,13 @@
+//
+//  iOSTimer.h
+//  iOSTimer
+//
+//  Created by Thomas Goetzsche on 05/11/2017.
+//  Copyright © 2017 net.tgotzsche. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSTimer : NSObject
+
+@end
---
+++ iOSTimer/iOSTimer.m
@@ -0,0 +1,13 @@
+//
+//  iOSTimer.m
+//  iOSTimer
+//
+//  Created by Thomas Goetzsche on 05/11/2017.
+//  Copyright © 2017 net.tgotzsche. All rights reserved.
+//
+
+#import "iOSTimer.h"
+
+@implementation iOSTimer
+
+@end
